f5c1a0e15a1ed90670d841fdc06d28b2751913ab
zifang0609 <313158051@qq.com>
2016-01-07 21:32:14 +0800

    dd

--- src/com/xj/github/test/prodcute.java
+++ src/com/xj/github/test/prodcute.java
@@ -3,4 +3,14 @@ package com.xj.github.test;
 public class prodcute {
 
 	private String  name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
