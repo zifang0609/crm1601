2f72b0431e8e98deb656d8753706438f7552bd25
zifang0609 <313158051@qq.com>
2016-01-07 20:54:13 +0800

    new entity

---
+++ src/com/xj/github/test/Customer.java
@@ -0,0 +1,24 @@
+package com.xj.github.test;
+
+public class Customer {
+
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", age=" + age + "]";
+	}
+	
+}
