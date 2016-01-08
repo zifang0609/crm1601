5850afdb273cd6920b7560d72d42559b094032f0
zifang0609 <313158051@qq.com>
2016-01-08 21:31:03 +0800

    update3

--- src/com/xj/crm/TestUpdate.java
+++ src/com/xj/crm/TestUpdate.java
@@ -5,5 +5,6 @@ public class TestUpdate {
 	public static void main(String[] args) {
 		System.out.println("ok update");
 			System.out.println("ok update2");
+			System.out.println("ok update3");
 	}
 }
